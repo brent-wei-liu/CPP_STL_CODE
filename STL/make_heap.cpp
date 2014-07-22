bb99ce8f965612bd12e04e01e09d9eb441d1c98e
Wei Liu <kissinguramii@gmail.com>
2014-07-22 11:45:45 -0400

    code

--- STL/make_heap.cpp
+++ STL/make_heap.cpp
@@ -5,30 +5,58 @@
 #include <functional>
  
 using namespace std;
- 
-void Push(vector<int>& heap, int val) {
-    heap.push_back(val);
-    push_heap(heap.begin(), heap.end(), greater<int>());
-}
-int Pop(vector<int>& heap) {
-    int val = heap.front();
-     
-    //This operation will move the smallest element to the end of the vector
-    pop_heap(heap.begin(), heap.end(), greater<int>());
- 
-    //Remove the last element from vector, which is the smallest element
-    heap.pop_back();  
-    return val;
-}
-int main() {
-    vector<int> heap;
-    Push(heap, 1);
-    Push(heap, 7);
-    Push(heap, 5);
- 
-    while (!heap.empty()) {
-         cout << Pop(heap) << endl;
+class Node{
+public:
+    int x, y;
+    Node( int a= 0, int b= 0 ):x(a),y(b) {}
+    bool operator<(Node m)const{
+        if( x == m.x)
+            return y < m.y;
+        return x<m.x;
     }
+    void print()
+    {
+        cout<<"("<<x<<" ,"<<y<<")"<<endl;
+    }
+};
+
+class Heap{
+private:
+    vector<Node> heap;
+public:
+    void Push( Node val) {
+        heap.push_back(val);
+        push_heap(heap.begin(), heap.end());
+    }
+    Node Pop() {
+        Node val = heap.front();
+         
+        //This operation will move the smallest element to the end of the vector
+        pop_heap(heap.begin(), heap.end());
+     
+        //Remove the last element from vector, which is the smallest element
+        heap.pop_back();  
+        return val;
+    }
+    void print()
+    {
+        vector<Node>::iterator it = heap.begin();
+        for(; it!=heap.end(); it++){
+            it->print();
+        }
+    }
+};
+int main() {
+    Heap h;
+    h.Push( Node(1,2));
+    h.Push( Node(2,2));
+    h.Push( Node(4,3));
+    h.Push( Node(2,3));
+    h.Push( Node(1,3));
+    h.Push( Node(2,7));
+    h.Push( Node(1,4));
+    h.Push( Node(2,1));
+    h.print(); 
     return 0;
 }
 
